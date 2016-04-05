accf5642f0ef961c5323b0be15c50057a04c9ba9
Takahiro Kamiy <takahiro_kamiya@esrij.com>
2016-04-06 00:47:31 +0900

    Update Feature.js

--- viewer/js/gis/dijit/LayerControl/controls/Feature.js
+++ viewer/js/gis/dijit/LayerControl/controls/Feature.js
@@ -26,6 +26,9 @@ define([
             } else {
                 this._expandRemove();
             }
+        },
+        destroy: function () {
+            this.inherited(arguments);
         }
     });
     return FeatureControl;
